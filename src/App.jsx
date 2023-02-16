3aa8e41c6426ef971915b801afa0ecb1e58ffb7c
Vlada-WoodyChick <uchusa@mail.ru>
2023-02-16 12:55:28 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import { StartPage } from './components/StartPage/StartPage'
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      
+      <StartPage/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/StartPage/StartPage.jsx
@@ -0,0 +1,29 @@
+import "./style.css";
+
+export const StartPage = () => {
+  return (
+    <body>
+      <div className="mainCover">
+        <div className="mainStartPage">
+          <h1 className="gameName">
+            <div className="word word_1">Помогите бабушке</div>            
+            <div className="word word_2">нарядить елку</div>
+           
+            
+            </h1>
+        </div>
+        
+        <button className="startGame">Начать игру</button>
+      </div>
+
+      <footer className="footerMenuMainCover"></footer>
+    </body>
+  );
+};
+<button class="to-game reset">Начать игру</button>;
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Vlada-WoodyChick/learnReact.git
+git push -u origin main
