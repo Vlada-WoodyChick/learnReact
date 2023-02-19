fc2bccae9a19a9177ed8bac25ee077862e0b61bf
Vlada-WoodyChick <uchusa@mail.ru>
2023-02-19 20:56:48 +0300

    19.02.23 21.00

--- src/components/StartPage/StartPage.jsx
+++ src/components/StartPage/StartPage.jsx
@@ -1,23 +1,27 @@
 import "./style.css";
-
+/* npm run dev */
+/* Границы будут одинаковые на всем main cover  странице класс borderMain
+ */
 export const StartPage = () => {
   return (
     <body>
-      <div className="mainCover">
-        <div className="mainStartPage">
-          <h1 className="gameName">
-            <div className="word word_1">Помогите бабушке</div>            
-            <div className="word word_2">нарядить елку</div>
-           
-            
-            </h1>
+      <div className="mainCover"></div>
+      <div className="rectangle1272">
+        <h1> <div className="gameName">
+          <div className="words_11">Помогите бабушке</div>
+          <div className="words_12">нарядить елку</div>
         </div>
-        
-        <button className="startGame">Начать игру</button>
+        </h1>
       </div>
+
+      <button className="rectangle1273">
+        <div className="startGameText">Начать игру</div>
+      </button>
+      <div className="ballImage_1"></div>
+      <div className="ballImage_2" ></div>
 
       <footer className="footerMenuMainCover"></footer>
     </body>
   );
 };
-<button class="to-game reset">Начать игру</button>;
+/*<button class="to-game reset">Начать игру</button>; */
