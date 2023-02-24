520dcf4a6a59bd1cd071cfcb7ce022dfa058baf2
Vlada-WoodyChick <uchusa@mail.ru>
2023-02-24 21:55:45 +0300

    24.02.23 21.53 commit

--- src/components/StartPage/StartPage.jsx
+++ src/components/StartPage/StartPage.jsx
@@ -1,11 +1,45 @@
 import "./style.css";
+import FooterLogo from "./../../assets/rs_school_js 1.png";
+import BallMain_1 from "./../../assets/ball_main_1.png";
+
+
+import MainCoverPicture from "./../../assets/annie_spratt_unsplash.png";
 /* npm run dev */
 /* Границы будут одинаковые на всем main cover  странице класс borderMain
  */
 export const StartPage = () => {
   return (
-    <body>
-      <div className="mainCover"></div>
+    <div className="wrapper">
+      <img
+        src={MainCoverPicture}
+        alt="Еловые лапы"
+        className="mainCoverPicture"
+      />
+
+      <div className="toys">
+        <img src={BallMain_1} alt="Шар елочный" className="ballMain_1" />
+        <img src={BallMain_1} alt="Шар елочный" className="ballMain_2" />
+        
+      </div>
+      <div className="contanierGameTitle borderMain">
+        <h1 className="contanierH1">
+          <span className="textH1">Помогите бабушке нарядить елку </span>
+        </h1 >
+        </div>
+       
+        <button className="startGameButton borderMain">
+          <span  className="startGameText">Начать</span></button>
+      <div className="footer">
+        <div className="footer_autor_contanier">
+          <span className="footer_autor_text">Vlada</span>
+          </div>
+        
+         <div className="footer_logo_cont">
+          <img src={FooterLogo} alt="Logo" className="footer_logo" />
+        </div> 
+      </div>
+    </div>
+    /*{<div className="mainCover"></div>
       <div className="rectangle1272">
         <h1> <div className="gameName">
           <div className="words_11">Помогите бабушке</div>
@@ -20,8 +54,7 @@ export const StartPage = () => {
       <div className="ballImage_1"></div>
       <div className="ballImage_2" ></div>
 
-      <footer className="footerMenuMainCover"></footer>
-    </body>
+      <footer className="footerMenuMainCover"></footer> } */
   );
 };
 /*<button class="to-game reset">Начать игру</button>; */
