27ccfd314d67d5cf4e517ed460643c542a43a9f6
Vlada-WoodyChick <uchusa@mail.ru>
2023-03-01 00:01:12 +0300

    01.03.23 00.01

---
+++ src/components/SecondPage/SecondPage.jsx
@@ -0,0 +1,11 @@
+<div className="container-main">
+  <div className="container-for-toys-and-text">
+    <span className="container-for-toys-and-text__text-toys"></span>
+    <div className="flex-container-for-toy-cards">
+      <div
+        className="flex-container-for-toy-cards__toy-card toy-card_row1-place1-card-8">
+            
+        </div>
+    </div>
+  </div>
+</div>;
