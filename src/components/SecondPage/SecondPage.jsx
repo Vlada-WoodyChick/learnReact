4b07c97dd00f2b3c88e5f6b03fcd229c63424b14
Vlada-WoodyChick <uchusa@mail.ru>
2023-03-11 21:45:04 +0300

    11.03.23 21.44 last true commit

--- src/components/SecondPage/SecondPage.jsx
+++ src/components/SecondPage/SecondPage.jsx
@@ -1,11 +1,36 @@
-<div className="container-main">
-  <div className="container-for-toys-and-text">
-    <span className="container-for-toys-and-text__text-toys"></span>
-    <div className="flex-container-for-toy-cards">
-      <div
-        className="flex-container-for-toy-cards__toy-card toy-card_row1-place1-card-8">
-            
+/* npm run dev */
+import "./style.css";
+import { toys } from "./constants";
+
+export const SecondPage = () => {
+  const toysComponets = toys.map((toy, index) => {
+    return (
+      <div className="flex-container-for-toy-cards__toy-card">
+        <span>{toy.name}</span>
+        <img src="" alt="" />
+        <span>{toy.count}</span>
+        <span>{toy.year}</span>
+        <span>{toy.shape}</span>
+        <span>{toy.color}</span>
+        <span>{toy.size}</span>
+        <span></span>
+      </div>
+    );
+  });
+  return (
+    <div className="toys">
+      <div className="container-for-second-page">
+        <div className="container-for-forms border"></div>
+        <div className="container-for-toys-and-text border">
+          <span className="container-for-toys-and-text__text-toys">Игрушки</span>
+          <div className="flex-container-for-toy-cards">
+            <div className="flex-container-for-toy-cards__toy-card">
+              {toysComponets}
+            </div>
+          </div>
         </div>
+      </div>
+      <div className="footer"></div>
     </div>
-  </div>
-</div>;
+  );
+};
--- src/components/StartPage/StartPage.jsx
+++ src/components/StartPage/StartPage.jsx
@@ -2,7 +2,6 @@ import "./style.css";
 import FooterLogo from "./../../assets/rs_school_js 1.png";
 import BallMain_1 from "./../../assets/ball_main_1.png";
 
-
 import MainCoverPicture from "./../../assets/annie_spratt_unsplash.png";
 /* npm run dev */
 /* Границы будут одинаковые на всем main cover  странице класс borderMain
@@ -19,24 +18,25 @@ export const StartPage = () => {
       <div className="toys">
         <img src={BallMain_1} alt="Шар елочный" className="ballMain_1" />
         <img src={BallMain_1} alt="Шар елочный" className="ballMain_2" />
-        
       </div>
       <div className="contanierGameTitle borderMain">
         <h1 className="contanierH1">
           <span className="textH1">Помогите бабушке нарядить елку </span>
-        </h1 >
-        </div>
-       
-        <button className="startGameButton borderMain">
-          <span  className="startGameText">Начать</span></button>
+        </h1>
+      </div>
+
+      <button className="startGameButton borderMain">
+        <a href="SecondPage.jsx"><span className="startGameText">Начать</span></a>
+        
+      </button>
       <div className="footer">
         <div className="footer_autor_contanier">
           <span className="footer_autor_text">Vlada</span>
-          </div>
-        
-         <div className="footer_logo_cont">
+        </div>
+
+        <div className="footer_logo_cont">
           <img src={FooterLogo} alt="Logo" className="footer_logo" />
-        </div> 
+        </div>
       </div>
     </div>
     /*{<div className="mainCover"></div>
