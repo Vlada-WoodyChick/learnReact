1d8c064e898089227f3422814b005c58e67583a1
Vlada-WoodyChick <uchusa@mail.ru>
2023-03-12 21:22:04 +0300

    12.03.23 21.21

--- src/components/SecondPage/SecondPage.jsx
+++ src/components/SecondPage/SecondPage.jsx
@@ -6,31 +6,35 @@ export const SecondPage = () => {
   const toysComponets = toys.map((toy, index) => {
     return (
       <div className="flex-container-for-toy-cards__toy-card">
-        <span>{toy.name}</span>
+        <span className="toy-card toy-card_name">{toy.name}</span>
         <img src="" alt="" />
-        <span>{toy.count}</span>
-        <span>{toy.year}</span>
-        <span>{toy.shape}</span>
-        <span>{toy.color}</span>
-        <span>{toy.size}</span>
-        <span></span>
+        <span className="toy-card toy-card_count" > Количество:  {toy.count}</span>
+        <span className="toy-card toy-card_year" >Год покупки: {toy.year}</span>
+        <span className="toy-card toy-card_shape"> Форма игрушки: {toy.shape}</span>
+        <span className="toy-card toy-card_color">Цвет игрушки: {toy.color}</span>
+        <span className="toy-card toy-card_size">Размер игрушки: {toy.size}</span>
+        <span className="toy-card toy-card_do-like">Любимая: </span>
       </div>
     );
   });
+  
   return (
     <div className="toys">
       <div className="container-for-second-page">
         <div className="container-for-forms border"></div>
         <div className="container-for-toys-and-text border">
-          <span className="container-for-toys-and-text__text-toys">Игрушки</span>
+          <span className="container-for-toys-and-text__text-toys">
+            Игрушки
+            </span>
           <div className="flex-container-for-toy-cards">
-            <div className="flex-container-for-toy-cards__toy-card">
-              {toysComponets}
+          {toysComponets}
+          <div className="flex-container-for-toy-cards__toy-card">
+          
             </div>
           </div>
         </div>
       </div>
-      <div className="footer"></div>
+      <div className="footer__second-page"></div>
     </div>
   );
 };
